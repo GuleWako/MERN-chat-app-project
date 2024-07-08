4102d2ce0710ca3786f4e9c52304f7fcb1d840bb
Gule <gulumawakuma70@gmail.com>
2024-07-08 10:41:00 +0300

    add image

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Login from "./pages/login/Login";
 
 const App = () => {
   return (
-    <div className=''>App</div>
-  )
-}
+    
+   <div className=" p-4 flex items-center justify-center h-screen">
+    <Login/>
+   </div>
+    
+  );
+};
 
-export default App
+export default App;
